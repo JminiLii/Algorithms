c60e68875aa2612f2700aa4f4cf6f18bd2da9598
JminiLii <103085094+JminiLii@users.noreply.github.com>
2022-08-12 20:44:13 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		int num2 = in.nextInt();
+		System.out.println(num+num2);
+	}
+
+}
